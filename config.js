b9ae0fa39a1b4525466cd28e9a63f9c22db20241
aashifserbot-hub <aashifserbot@gmail.com>
2026-03-09 11:43:37 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+export const OWNER_NUMBER = "94768655794"
+export const OWNER_NAME = "AASHIF SER"
+
+export const BOT_NAME = "AASHIF-MD V1"
+export const BOT_IMAGE = "https://i.ibb.co/xqtTS4B5/Lte4-Pl-Nbqa.jpg"
+
+export const DEV_NAME = "AASHIF SER"
+export const DEV_NUMBER = "94768655794"
+
+export const PREFIX = "."
